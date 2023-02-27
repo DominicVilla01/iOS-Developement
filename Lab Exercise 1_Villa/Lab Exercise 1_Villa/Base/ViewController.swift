bd7177cc62ca5faf88c8270ec51e2ac170b8c235
DominicVilla01 <88145935+DominicVilla01@users.noreply.github.com>
2023-02-27 17:03:15 +0800

    add lab exercise 1 in the repository

---
+++ Lab Exercise 1_Villa/Lab Exercise 1_Villa/Base/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab Exercise 1_Villa
+//
+//  Created by student on 2/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
